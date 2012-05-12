b3369fa3d8bee0628234d53abc4afe949d0640a8
Gosuke Miyashita <gosukenator@gmail.com>
2012-05-12 21:47:43 +0900

    sync するコマンドも追加

--- add_members.sh
+++ add_members.sh
@@ -4,3 +4,5 @@ while read USER; do
     echo $USER
     thor member:add --user=$USER --organization=paperboy-all --team=Members --public
 done <"members.txt"
+
+thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-sqale --destteam=paperboy
