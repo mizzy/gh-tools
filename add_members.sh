4799d00d4aaddf914d86dbb8e0b6de74847ffa93
Gosuke Miyashita <gosukenator@gmail.com>
2012-05-14 12:25:05 +0900

    paperboy-fanic にも同期するようにした。

--- add_members.sh
+++ add_members.sh
@@ -2,3 +2,4 @@
 
 thor member:bulk_add --file=$1 --organization=paperboy-all --team=Members --public
 thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-sqale --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-fanic --destteam=paperboy
