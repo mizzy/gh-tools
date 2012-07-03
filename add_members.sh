6c7d1dda295ff518190f6e4c97e29db6be546fe4
Gosuke Miyashita <gosukenator@gmail.com>
2012-07-03 09:32:19 +0900

    すべてのペパボ組織アカウントで、全員が所属するチームをpaperboyに統一する

--- add_members.sh
+++ add_members.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-thor member:bulk_add --file=$1 --organization=paperboy-all --team=Members --public
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-sqale --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-fanic --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-petit --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-30days --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-heteml --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-app --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-ec-shien --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-minne --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-jugemcart --destteam=paperboy
-thor member:sync --srcorg=paperboy-all --srcteam=Members --destorg=paperboy-muumuu-domain --destteam=paperboy
+thor member:bulk_add --file=$1 --organization=paperboy-all --team=paperboy --public
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-sqale --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-fanic --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-petit --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-30days --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-heteml --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-app --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-ec-shien --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-minne --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-jugemcart --destteam=paperboy
+thor member:sync --srcorg=paperboy-all --srcteam=paperboy --destorg=paperboy-muumuu-domain --destteam=paperboy
